96ccf8893c40acb3ecb0119d127d31fdbe6004d0
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2015-09-17 13:33:54 +0200

    minified js

--- config/config.php
+++ config/config.php
@@ -34,10 +34,10 @@ if (TL_MODE == 'BE')
 	$GLOBALS['TL_JAVASCRIPT']['jquery-noconflict'] = 'system/modules/tagsinput/assets/js/jquery-noconflict.js';
 }
 
-$GLOBALS['TL_JAVASCRIPT']['tagsinput'] = 'system/modules/tagsinput/assets/vendor/bootstrap-tagsinput/dist/bootstrap-tagsinput' . (!$GLOBALS['TL_CONFIG']['debugMode'] ? '.min' : '') . '.js';
-$GLOBALS['TL_JAVASCRIPT']['typeahead'] = 'system/modules/tagsinput/assets/vendor/typeahead.js/dist/typeahead.bundle' . (!$GLOBALS['TL_CONFIG']['debugMode'] ? '.min' : '') . '.js';
+$GLOBALS['TL_JAVASCRIPT']['tagsinput'] = 'system/modules/tagsinput/assets/vendor/bootstrap-tagsinput/dist/bootstrap-tagsinput' . (!$GLOBALS['TL_CONFIG']['debugMode'] ? '.min' : '') . '.js|static';
+$GLOBALS['TL_JAVASCRIPT']['typeahead'] = 'system/modules/tagsinput/assets/vendor/typeahead.js/dist/typeahead.bundle' . (!$GLOBALS['TL_CONFIG']['debugMode'] ? '.min' : '') . '.js|static';
 
-$GLOBALS['TL_JAVASCRIPT']['tagsinput-be'] = 'system/modules/tagsinput/assets/js/jquery.tagsinput.js';
+$GLOBALS['TL_JAVASCRIPT']['tagsinput-be'] = 'system/modules/tagsinput/assets/js/jquery.tagsinput' . (!$GLOBALS['TL_CONFIG']['debugMode'] ? '.min' : '') . '.js|static';
 
 /**
  * CSS
