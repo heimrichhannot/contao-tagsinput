da5a7824a406f3416d5f23aff0c13d17af12bc4e
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2024-02-13 17:39:18 +0100

    add copyright notice to HeimrichHannotContaoTagsInput.php

--- src/HeimrichHannotContaoTagsInput.php
+++ src/HeimrichHannotContaoTagsInput.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Copyright (c) 2024 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
+ */
+
 namespace HeimrichHannot\TagsInput;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
