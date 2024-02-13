0ed7f48decad9cbcb999c31a00c2100e7de487ce
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2024-02-13 14:14:55 +0100

    current state

--- src/HeimrichHannotContaoTagsInput.php
+++ src/HeimrichHannotContaoTagsInput.php
@@ -6,4 +6,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class HeimrichHannotContaoTagsInput extends Bundle
 {
+    public function getPath()
+    {
+        return dirname(__DIR__);
+    }
+
 }
